9eacec8d652765d3ea23e7eea1b987f11be58d6c
p-sajju <50116631+p-sajju@users.noreply.github.com>
2023-12-16 19:08:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// code readability should be high
+// In javascript we have different types of datatypes.
+
+let name = "Sajjan"  // String
+let age = 20 // Number , bigint for the larger numbers.
+let isLoggedIn = true // boolean
+let xyz = null // null is also a data type as well as we use it to declare a variable.
+let person = {id: 1, name: "Demo"}; // object 
+let xyx; // undefined is also a datatype in js.
+// We have sysmbols as well in js.
+
+console.log(typeof (null)); // type of null is a object.
+console.log(typeof (undefined)); // type of undefined is undefined.
+console.log(typeof (Object)); // type of Object is function.
