b4c8c53762ed1b2bebdac66542aa6d848b3614b1
p-sajju <50116631+p-sajju@users.noreply.github.com>
2023-12-17 06:37:24 +0000

    Operation

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,9 @@
+//************Operations**********//
+
+console.log(2+2); //Addition
+console.log(2-2); //Subt
+console.log(2*2); // Multiply
+console.log(2/2); // divide
+console.log(2**5); // raise to the power
+console.log(2%5); // Modulus returns remainder.
+// For More Information you can visit this site https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values.
