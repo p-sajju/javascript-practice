b761738603c166c623c5ae66dd929261c4a171c9
p-sajju <50116631+p-sajju@users.noreply.github.com>
2023-12-17 07:49:28 +0000

    Operation

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -7,3 +7,29 @@ console.log(2/2); // divide
 console.log(2**5); // raise to the power
 console.log(2%5); // Modulus returns remainder.
 // For More Information you can visit this site https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values.
+
+
+
+//*******Comparisions*******//
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // comparision converts null to a number considered as 0 that's why null > 0 is fallse and null >= 0 true.
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
